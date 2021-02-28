7467b51166ee242c9f74cdb968188858afd405a3
mpiorowski <mateuszpiorowski@gmail.com>
2021-02-28 18:33:53 +0100

    finished part 6

--- api-gateway/src/server/startServer.ts
+++ api-gateway/src/server/startServer.ts
@@ -1,6 +1,10 @@
+import { resolvers } from "#root/graphql/resolvers";
+import { schema } from "#root/graphql/schema";
 import accessEnv from "#root/helpers/accessEnv";
 import { ApolloServer } from "apollo-server-express";
 import config from "config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import { formatGraphQLErrors } from "./formatGraphQLErrors";
 
@@ -8,13 +12,19 @@ const PORT = config.get("PORT") as number;
 
 export const startServer = () => {
   const app = express();
+  app.use(cookieParser());
+
+  // different settings
+  app.use(cors());
 
   const apolloServer = new ApolloServer({
-    context: a=> a,
+    context: (a) => a,
     formatError: formatGraphQLErrors,
-    resolvers,
-    typeDefs: scheme
-  })
+    resolvers: resolvers,
+    typeDefs: schema,
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false, path: "/graphql" });
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`api gateway listing on ${PORT}`);
